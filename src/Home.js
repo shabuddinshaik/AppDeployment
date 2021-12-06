49dee402b10b8e7270dd3447c8dd44dbd34a9cec
root <root@ip-172-31-34-244.us-east-2.compute.internal>
2021-12-06 05:59:50 +0000

    Home.js is modified

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2>Welcome!!</h2>
+        <h2>Welcome shaik!!</h2>
         <p>we will use this app to see different ways to deploy a React App </p>
  
         <p>This is a single page app using React</p>
